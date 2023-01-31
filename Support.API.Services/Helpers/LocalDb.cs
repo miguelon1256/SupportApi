4380bc2bc161cc24fa3c4b96d55a11ca88df2df2
Your Name <you@example.com>
2023-01-31 13:16:51 -0800

    addNewChanges

--- Support.API.Services/Helpers/LocalDb.cs
+++ Support.API.Services/Helpers/LocalDb.cs
@@ -12,13 +12,13 @@ public static class LocalDb
         { "SUPPORT_DB_USER", "user" },
         { "SUPPORT_DB_PASSWORD", "password" },
 
-        { "KOBO_DB_SERVER", "192.168.5.151" },
+        { "KOBO_DB_SERVER", "192.168.59.129" },
         { "KOBO_DB_PORT", "5435"},
         { "KOBO_DB_NAME", "koboform"},
         { "KOBO_CAT_DB_NAME", "kobocat"},
         { "KOBO_DB_USER", "kobo"},
         { "KOBO_DB_PASSWORD", "password"},
 
-        { "KOBO_API_SERVER", "http://kf.prias.mmaya.gob.bo" }
+        { "KOBO_API_SERVER", "http://kf.mmaya.gob.bo" }
     };
 }
